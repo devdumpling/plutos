a0bf5b52e97a4afaca2af660b251fe65d81ef2c2
Luke <luke@67-194-26-199.wireless.umnet.umich.edu>
2013-09-21 17:16:14 -0400

    osca & cleaning

--- db.js
+++ db.js
@@ -2,11 +2,26 @@ var neo4j = require('neo4j');
 var db = new neo4j.GraphDatabase('http://localhost:7474');
 
 //to make a node
-var node = db.createNode({name: 'TANK'});     // instantaneous, but...
-node.save(function (err, node) {    // ...this is what actually persists.
-    if (err) {
-        console.err('Error saving new node to database:', err);
-    } else {
-        console.log('Node saved to database with id:', node.id);
-    }
+var tank = db.createNode({name: 'Tank'}),
+	hark = db.createNode({name: 'Hark'}),
+	osca = db.createNode({name: 'OSCA'});
+
+tank.createRelationshipFrom(osca, 'subsumes', function(err, relationship) {
+	console.log(err, relationship);
 });
+
+hark.createRelationshipFrom(osca, 'subsumes', function(err, relationship) {
+	console.log(err, relationship);
+});
+
+var nodes = [tank, hark, osca];
+
+for (var i = nodes.length - 1; i >= 0; i--) {
+	nodes[i].save(function (err, node) {    // ...this is what actually persists.
+	    if (err) {
+	        console.err('Error saving new node to database:', err);
+	    } else {
+	        console.log('Node saved to database with id:', node.id);
+	    }
+	});
+};
